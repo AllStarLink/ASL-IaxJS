9b76b8f5264c0724c8409c9f015b6856937bfa0b
Rob Vella <me@robvella.com>
2020-09-07 22:42:00 -0700

    Refactoring. Prepare for call tracking

--- app.js
+++ app.js
@@ -1,13 +1,10 @@
 'use strict'
 
-const Server = require('./lib/server.js');
-const server = Server.init();
+const Server = require('./lib/Server.js');
+const RegisterApi = require('./lib/RegisterApi.js');
 const _ = require('lodash');
 
-// emits on new datagram msg
-server.on('message', function (msg, info) {
-    Iax.receiveMessage(msg, info);
-});
+Server.init(Iax.receiveMessage);
 
 const Iax = {
     calls: [],
@@ -23,7 +20,7 @@ const Iax = {
         REGREJ: 16,
         REGREL: 17,
         POKE: 30,
-        CALLTOKEN: 0x28
+        CALLTOKEN: 40
     },
     IE: {
         USERNAME: 6,
@@ -34,7 +31,7 @@ const Iax = {
         PEERADDRESS: 18,
         REFRESH: 19,
         DATETIME: 31,
-        CALLTOKEN: 0x36
+        CALLTOKEN: 54
     },
     receiveMessage(msg, info) {
         console.log('Received %d bytes from %s:%d\n', msg.length, info.address, info.port);
@@ -53,29 +50,37 @@ const Iax = {
         console.log('Timestamp:', parsed.timeStamp);
         // console.log('Outbound Seq No:', parsed.outboundSeqNo);
         // console.log('Inbound Seq No:', parsed.inboundSeqNo);
-        console.log('CMD:', this.getCmdType(parsed.cmdType));
+        console.log('CMD:', this.getCmdType(parsed.cmdType) || "UNKNOWN");
 
         // Information elements
         let infoElements = [];
         this.parseInfoElements(msg.slice(12), infoElements);
         parsed.infoElements = infoElements;
 
+        // Look to see if there's an existing entry for this call
+        parsed.call = this.retrieveCall(parsed);
+
         switch (parsed.cmdType) {
+            case this.CMD.REGREL:
+                // TODO
+                this.regReleaseResponse(parsed, info);
+                break;
             case this.CMD.REGREQ:
                 this.processRegRequest(parsed, info);
                 break;
             case this.CMD.PING:
+            case this.CMD.POKE:
                 this.pongResponse(parsed, info);
                 break;
             case this.CMD.LAGRQ:
                 this.lagResponse(parsed, info);
             default:
+                // Just ignore them
                 break;
         }
     },
     processRegRequest(inMsg, senderInfo) {
         let verifyRegReq = false;
-
         /*
             Check to see if this is an initial REGREQ, or an MD5 salted one.
             The first request from Asterisk is REGREQ, which the server responds
@@ -98,31 +103,32 @@ const Iax = {
     },
     regRequestResponse(inMsg, senderInfo) {
         let sourceDestArr = new Uint16Array([
-            this.addBitwise(32768, this.generateRandomNumber(1000,2000)),
-            inMsg.scall,
+            // Turns this into 0x8001. This would normally be easy but JS doesn't have strong typing
+            // or I'm stupid
+            this.addBitwise(32768, this.generateRandomNumber(1, 65534)),
+            inMsg.call.src,
             0,
             1
         ]);
 
         let buffer = Buffer.from(sourceDestArr.buffer).swap16();
-
-        let randomChallenge = this.generateRandomNumber(100000000,999999999);
+        let randomChallenge = this.generateRandomNumber(100000000, 999999999);
         let username = this.getNode(inMsg.infoElements);
-        
+
         let outMsg = new Uint8Array([
-            0,
-            1, // (++inMsg.inboundSeqNo),
+            inMsg.call.outboundSeqNo,
+            inMsg.call.inboundSeqNo,
             6, // IAX
-            Iax.CMD.REGAUTH,
-            Iax.IE.AUTHMETHODS,
+            this.CMD.REGAUTH,
+            // IE Begin
+            this.IE.AUTHMETHODS,
             2, // Length of Auth Method
             0, // Auth method
             3, // Auth method
-            Iax.IE.MD5CHALLENGE,
-            9,
-            // Random # begin
+            this.IE.MD5CHALLENGE,
+            9, // MD5 Challenge length
             ...Buffer.from(String(randomChallenge)),
-            6,
+            this.IE.USERNAME,
             username.length,
             ...Buffer.from(String(username))
         ]);
@@ -132,8 +138,8 @@ const Iax = {
     },
     verifyRegRequest(inMsg, senderInfo) {
         let sourceDestArr = new Uint16Array([
-            inMsg.dcall,
-            inMsg.scall,
+            inMsg.call.dest,
+            inMsg.call.src,
             0,
             1
         ]);
@@ -143,12 +149,13 @@ const Iax = {
 
         // Parse IP for sending back PEERADDRESS
         let ipArr = senderInfo.address.match(/(\d{1,3})/g);
-        
+
         let testArr = [
-            (inMsg.inboundSeqNo),
-            (++inMsg.outboundSeqNo),
+            inMsg.call.outboundSeqNo,
+            inMsg.call.inboundSeqNo,
             6, // IAX
             this.CMD.REGACK,
+            // IE Begin
             this.IE.USERNAME,
             username.length,
             ...Buffer.from(String(username)),
@@ -162,14 +169,13 @@ const Iax = {
             // DateTime END
             this.IE.REFRESH,
             2, // Length
-            0x00,
-            5, // REFRESH TIME
+            0, // REFRESH time
+            5, // REFRESH time
             this.IE.PEERADDRESS,
             16, // Length
-            2, // Family?
-            0,
+            ...[0x02, 0x00], // Family FIXME
             ...this.splitIntToByteArray(senderInfo.port),
-            ...ipArr,
+            ...ipArr, // Client's IP
             // Padding probably for IPv6
             0,
             0,
@@ -180,7 +186,7 @@ const Iax = {
             0,
             0
         ];
-        
+
         let outMsg = new Uint8Array(testArr);
         console.log(outMsg);
         let outMsgBuf = Buffer.concat([buffer, Buffer.from(outMsg)]);
@@ -188,10 +194,19 @@ const Iax = {
         Server.send(outMsgBuf, senderInfo.address, senderInfo.port);
 
     },
+    regReleaseResponse(inMsg, senderInfo) {
+
+    },
+    /**
+     * This command only needs the CMD and nothing else to respond
+     *
+     * @param inMsg
+     * @param senderInfo
+     */
     pongResponse(inMsg, senderInfo) {
         let sourceDestArr = new Uint16Array([
-            inMsg.dcall,
-            inMsg.scall,
+            inMsg.call.dest,
+            inMsg.call.src,
             0,
             1
         ]);
@@ -199,19 +214,24 @@ const Iax = {
         let buffer = Buffer.from(sourceDestArr.buffer).swap16();
 
         let outMsg = new Uint8Array([
-            (inMsg.inboundSeqNo),
-            (++inMsg.outboundSeqNo),
-            6,
-            Iax.CMD.PONG,
+            inMsg.call.outboundSeqNo,
+            inMsg.call.inboundSeqNo,
+            6, // IAX
+            this.CMD.PONG,
         ]);
 
         let outMsgBuf = Buffer.concat([buffer, Buffer.from(outMsg)]);
         Server.send(outMsgBuf, senderInfo.address, senderInfo.port);
     },
+    /**
+     * This command differs slightly from PONG, in that it needs the timestamp from the client repeated back
+     * @param inMsg
+     * @param senderInfo
+     */
     lagResponse(inMsg, senderInfo) {
         let sourceDestArr = new Uint16Array([
-            inMsg.dcall,
-            inMsg.scall,
+            inMsg.call.dest,
+            inMsg.call.src,
         ]);
 
         let tArr = new Uint32Array([inMsg.timeStamp]);
@@ -219,10 +239,10 @@ const Iax = {
         let tBuffer = Buffer.from(tArr.buffer).swap32();
 
         let outMsg = new Uint8Array([
-            (++inMsg.inboundSeqNo),
-            (++inMsg.outboundSeqNo),
-            6,
-            Iax.CMD.LAGRP,
+            inMsg.call.outboundSeqNo,
+            inMsg.call.inboundSeqNo,
+            6, // IAX
+            this.CMD.LAGRP,
         ]);
 
         let outMsgBuf = Buffer.concat([buffer, tBuffer, Buffer.from(outMsg)]);
@@ -241,7 +261,7 @@ const Iax = {
         });
 
         type = this.getInfoType(type);
-        console.log("Info Type", type);
+        console.log("Info Type", type || 'UNKNOWN');
         console.log("Length", length);
         console.log("Data");
 
@@ -256,16 +276,16 @@ const Iax = {
     },
     getNode(infoElements) {
         let node = null;
-      
+
         _.each(infoElements, (v) => {
-            if (v.type !== this.IE.USERNAME) return null;
+            if (v.type !== this.IE.USERNAME) return;
             node = v.data;
         });
-        
+
         return node;
     },
     getCmdType(type) {
-        let cmdType = 'UNKNOWN';
+        let cmdType = null;
 
         _.each(this.CMD, (v, k) => {
             if (v !== type) return;
@@ -275,7 +295,7 @@ const Iax = {
         return cmdType;
     },
     getInfoType(type) {
-        let infoType = 'UNKNOWN';
+        let infoType = null;
 
         _.each(this.IE, (v, k) => {
             if (v !== type) return;
@@ -283,6 +303,9 @@ const Iax = {
         })
 
         return infoType;
+    },
+    retrieveCall(parsed) {
+
     },
     addBitwise(a, b, subtract = false) {
         while (b !== 0) {
@@ -310,10 +333,10 @@ const Iax = {
             .match(/.{1,2}/g)
             .map(b => parseInt(b.padStart(4, '0x')));
     },
-    bufferToHex (buffer) {
-        return [...new Uint8Array (buffer)]
-            .map (b => b.toString (16).padStart (2, "0"))
-            .join ("");
+    bufferToHex(buffer) {
+        return [...new Uint8Array(buffer)]
+            .map(b => b.toString(16).padStart(2, "0"))
+            .join("");
     }
 };
 
