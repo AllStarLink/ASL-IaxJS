88d0905bd81ab6425bc06c7a050890be7fd6798b
Rob Vella <me@robvella.com>
2020-09-09 20:54:27 -0700

    Add license. Refactor. Setup main server instance to be clustered for parallelism. Create testClient for load testing. Switch to Redis instead of Maps for scaling & clustering. Convert Call and Iax to classes because of circular dependencies.

---
+++ iaxServer.js
@@ -0,0 +1,47 @@
+/**
+ * IAX2 Server for Registrations
+ *
+ * @author Rob Vella KK9ROB <me@robvella.com>
+ * @copyright AllStarLink, Inc
+ * @license GPL-3.0-only
+ */
+
+require('dotenv').config();
+
+const cluster = require('cluster');
+const colors = require('colors');
+const numCPUs = require('os').cpus().length;
+
+if (cluster.isMaster) {
+    // Fork workers.
+    for (let i = 0; i < numCPUs; i++) {
+        cluster.fork();
+    }
+
+    cluster.on('exit', (worker, code, signal) => {
+        console.log(`worker ${worker.process.pid} died`);
+    });
+} else {
+    const asyncRedis = require("async-redis");
+    const redis = asyncRedis.createClient({
+        host: process.env.REDIS_HOST,
+        port: process.env.REDIS_PORT,
+        // password: process.env.REDIS_PASSWORD || undefined
+    });
+   
+    const Server = require('./lib/Server.js');
+    const Iax = new (require('./lib/Iax'));
+    
+    redis.on('ready', async () => {
+        Iax.Call.setRedis(redis);
+
+        Server.init((msg, info) => {
+            try {
+                Iax.receiveMessage(msg, info);
+            } catch (e) {
+                console.error(e);
+            }
+        });
+    })
+}
+
