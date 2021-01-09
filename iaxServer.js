7a1d65ec65f809bbca69d09d910856d70c7ba2f4
Rob Vella <me@robvella.com>
2021-01-08 17:08:40 -0800

    Workaround for weird clients that flood

--- iaxServer.js
+++ iaxServer.js
@@ -10,7 +10,7 @@ require('dotenv').config({ path: __dirname + '/.env' });
 
 const cluster = require('cluster');
 const colors = require('colors');
-const numCPUs = require('os').cpus().length * 4;
+const numCPUs = require('os').cpus().length;
 
 if (cluster.isMaster) {
     // Fork workers.
